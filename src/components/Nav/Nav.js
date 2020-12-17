2d570ba2d6332fbae56237c45003e4eaee45ad81
brijesh <brijeshbumrela@gmail.com>
2020-12-17 13:03:55 +0530

    link to upload file

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,21 +1,36 @@
-import React from 'react';
-import Button from '../../UI/Button/Button';
-import Input from '../../UI/Input/Input';
-import styles from './Nav.module.scss';
+import React from "react";
+import Button from "../../UI/Button/Button";
+import styles from "./Nav.module.scss";
 
-const Nav = ({ onClick, onGenerate, onFileUpload }) => (
-    <ul className={styles.header}>
-        <li>
-            <h1>Equation Visualizer</h1>
-        </li>
-        <li>
-            <ul className={styles.navRightBtn}>
-                <li><Button onClick={onClick} text="Graph Settings"></Button></li>
-                <li><Button onClick={onGenerate} text="Generate JSON"></Button></li>
-                <li><Input onChange={onFileUpload} type="file" text="Upload Image"></Input></li>
-            </ul>
-        </li>
-    </ul>
-)
+const Nav = ({ onClick, onGenerate, onFileUpload, history }) => {
+
+    return (
+        <ul className={styles.header}>
+            <li>
+                <h1>Equation Visualizer</h1>
+            </li>
+            <li>
+                <ul className={styles.navRightBtn}>
+                    <li>
+                        <Button
+                            onClick={onClick}
+                            text="Graph Settings"
+                        ></Button>
+                    </li>
+                    <li>
+                        <Button
+                            onClick={onGenerate}
+                            text="Generate JSON"
+                        ></Button>
+                    </li>
+                    {/* <li><Input onChange={onFileUpload} type="file" text="Upload Image"></Input></li> */}
+                    <li>
+                        <a href="http://localhost:8000" target="_newtab">Upload</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    );
+};
 
 export default Nav;
